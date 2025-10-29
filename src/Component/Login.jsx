d65adaff3e10ba726f11000fed0bf737ecd32a3d
Arulmelviya205 <melviyarajan@gmail.com>
2025-10-29 12:59:23 +0530

    hos

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import '../Styles/Login.css'
 
+import hos from '../assets/images/hos.jpg'
+import login0 from '../assets/images/login0.jpg'
 export default function Login() {
+ 
   return (
     <div>
-      <h1>welcome</h1>
+      <h1 className='pit'>Hospital</h1>
+      <img className='pit0' src={hos}/>
+      <div className='dd'></div>
+      <img className='login0' src={login0}/>
+       <h1 className='gin'>login</h1>
+      
     </div>
   )
 }
