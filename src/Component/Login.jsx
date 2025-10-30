0df8891bbe9ce4e9927ff8f33c93198bfdcdf21e
Arulmelviya205 <melviyarajan@gmail.com>
2025-10-30 11:58:29 +0530

    first

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import '../Styles/Login.css'
-
+import qq from '../assets/images/qq.jpg'
 import hos from '../assets/images/hos.jpg'
 import login0 from '../assets/images/login0.jpg'
+import ma from '../assets/images/ma.jpg'
+import pass from '../assets/images/pass.jpg'
+import face from '../assets/images/face.jpg'
+import insta from '../assets/images/insta.jpg'
+import goo from '../assets/images/goo.jpg'
 export default function Login() {
  
   return (
@@ -11,8 +16,23 @@ export default function Login() {
       <img className='pit0' src={hos}/>
       <div className='dd'></div>
       <img className='login0' src={login0}/>
+      <img className='namk' src={qq}/>
+      <img className='mai1' src={ma}/>
+      <img className='ord1' src={pass}/>
        <h1 className='gin'>login</h1>
-      
+      <label className='bel'>Username:</label>
+      <input type='text'className='bel0'/>
+      <label className='mai'>Email:</label>
+      <input type='email' className='mai0'/>
+      <label className='ord'>Password:</label>
+      <input type='password' className='ord0'/>
+      <button type='submit' className='mit'>submit</button>
+      <div className='kn'></div>
+      <img className='fac' src={face}/>
+      <div className='kn0'></div>
+      <img className='ita' src={insta}/> 
+      <div className='kn1'></div>
+      <img className='goh' src={goo}/>
     </div>
   )
 }
