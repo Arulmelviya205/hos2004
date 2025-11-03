b1346e7195c3d666443e0adcab76e9bed655b8c6
Arulmelviya205 <melviyarajan@gmail.com>
2025-11-03 12:13:08 +0530

    pit

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Styles/Login.css'
 import qq from '../assets/images/qq.jpg'
 import hos from '../assets/images/hos.jpg'
@@ -9,7 +9,18 @@ import face from '../assets/images/face.jpg'
 import insta from '../assets/images/insta.jpg'
 import goo from '../assets/images/goo.jpg'
 export default function Login() {
- 
+     const [Email, setemail] = useState("");
+          
+            const submitEmail = () => {
+              if (Email === "") {
+                alert("Please enter an email.");
+              } else if (!Email.includes("@") || !Email.includes(".")) {
+                alert("Please enter a valid email address.");
+              } else {
+                alert("Submitted Successfully");
+                setEmail(""); 
+              }
+            }
   return (
     <div>
       <h1 className='pit'>Hospital</h1>
@@ -33,6 +44,35 @@ export default function Login() {
       <img className='ita' src={insta}/> 
       <div className='kn1'></div>
       <img className='goh' src={goo}/>
+      <h1 className='pi0kk'>Hospital</h1>
+      <img className='img01j' src={hos}/>
+      <div className='iik'></div>
+      <h1 className='nai'>Contact us</h1>
+      <p className='nai0'>📞 Phone: +91 98765 43210</p>
+      <p className='nai1'>📧 Email: support@hospital.com</p>
+      <p className='nai2'>📍 Address: 123, Anna Nagar, Chennai</p>
+      <img className='al' src={face}/>
+      <img className='al0' src={insta}/>
+      <img className='al1' src={goo}/>
+       <h1 className='kil'>Join us</h1>
+      <label className='kil0'>Email:</label>
+      <input type='email'value={Email}onChange={(e) => setemail(e.target.value)}className='kil1'/>
+      <button className='tok' onClick={submitEmail}>Submit</button>
+      <h2 className='jk'>Department</h2>
+      <h2 className='jk0'>Days</h2>
+      <h2 className='jk1'>Timing</h2>
+      <p className='ok'>General OPD</p>
+      <p className='ok0'>Monday-Saturday</p>
+      <p className='ok1'>8:00 AM – 8:00 PM</p>
+      <p className='pk'>Emergency</p>
+      <p className='pk0'>All Days</p>
+      <p className='pk1'>24 * 7</p>
+      <p className='pok'>Pharmacy</p>
+      <p className='pok0'>Monday-Sunday</p>
+      <p className='pok1'>7:00 AM – 10:00 PM</p>
+      <p className='ank'>Lab & Scan</p>
+      <p className='ank0'>Monday–Saturday</p>
+      <p className='ank1'>8:00 AM – 6:00 PM</p>
     </div>
   )
 }
